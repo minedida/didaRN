2c307411dfb0d91300805cf28252f33a768d651f
dautent <1446218781@qq.com>
2019-10-07 20:28:51 +0800

    chore: 调整demo目录结构

--- demo/Demo.tsx
+++ demo/Demo.tsx
@@ -1,73 +1,79 @@
-import React from 'react'
-import { Platform } from 'react-native'
-// @ts-ignore
-import TabViewExampleList from './react-native-tab-view/TabViewExampleList';
-// @ts-ignore
-import SortableListExample from './sort-and-swipe/react-native-sortable-list/example/SortableListExample';
-// @ts-ignore
-import SortableListViewExample from './sort-and-swipe/react-native-sortable-list/example/SortableListViewExample';
-// @ts-ignore
-import DragableListExample from './sort-and-swipe/react-native-sortable-list/example/DragableListExample';
-// @ts-ignore
-import SwipeableExample from './sort-and-swipe/react-native-swipeable/Example'
-// @ts-ignore
-import CustomSortAndSwipeList from './sort-and-swipe/react-native-sort-and-swipe-list/src/CustomSortAndSwipeList'
-// @ts-ignore
-import HumanShowcaseScreen from './react-native-typography/HumanShowcaseScreen'
-// @ts-ignore
-import MaterialShowcaseScreen from './react-native-typography/MaterialShowcaseScreen'
-// @ts-ignore
-import TypographyExample from './react-native-typography/TypographyExample'
-// @ts-ignore
-import Header from './Header/'
+import React, { useState, useEffect } from 'react'
+import { Platform, ScrollView, View } from 'react-native'
+import { List } from 'react-native-paper';
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+import DemoRouters from './DemoRouters';
+import { NavigationBar } from "../src/components";
 import SplashScreen from "react-native-splash-screen";
-// @ts-ignore
-import KeyboardDemoScreen from './keyboard-input/demoScreen';
-// @ts-ignore
-import RecycleTestComponent from './recyclerlistview/RecycleTestComponent'
-// @ts-ignore
-import StickySample from './recyclerlistview/StickySample'
-// @ts-ignore
-import NativeMaterialDialog from './material-dialog/NativeMaterialDialog'
-// @ts-ignore
-import SyanImagePicker from './image-picker/SyanImagePicker'
-// @ts-ignore
-import OfflineDemo from './offline/OfflineDemo'
-// @ts-ignore
-import RNShareDemo from './Share/RNShareDemo'
-// @ts-ignore
-import RNCShareDemo from './Share/RNCShareDemo'
-// @ts-ignore
-import Count1 from './hooks/Count1'
-// @ts-ignore
-import Count2 from './hooks/Count2'
-// @ts-ignore
-import Count3 from './hooks/Count3'
-// @ts-ignore
-import LightboxDemo from './light-demo/LightboxDemo'
-// @ts-ignore
-import ReanimatedCollapsibleNavbar from './collapsible/reanimated-collapsible-navbar/ReanimatedCollapsibleNavbar';
-// @ts-ignore
-import ReactNativeScrollableHeader from './collapsible/react-native-scrollable-header/ReactNativeScrollableHeader';
-// import ExpoExample from './expo/ExpoExample'
-// import RNScreensExample from './rn-screens/App';
-// @ts-ignore
-import SpecificPlatformExample from './specific-platform';
-// import SpecificPlatformTSExample from './specific-platform-ts';
-import FlutterAnimation from './fluttter-animations/'
 
-// @ts-ignore
-import MerryPhotoViewer from './@merryjs/photo-viewer/MerryPhotoViewer';
+const MapView = (props) => (
+  <>
+    {
+      DemoRouters.map((item: any, index) =>
+        <List.Item
+          key={item.displayName}
+          title={item.displayName}
+          onPress={() => props.onItemPress(index)}
+        />
+      )
+    }
+  </>
+);
+const NavigationView = ({initialRouteName = ''}) => {
+  const routeConfigMap = DemoRouters.reduce((p, c) => {
+    p[c.displayName] = c;
+    return p;
+  }, {});
+  const Navigation = createAppContainer(
+    createStackNavigator(
+      routeConfigMap,
+      {
+        initialRouteName,
+        headerMode: 'none',
+      })
+  );
+  return <Navigation />;
+};
 
-class Demo extends React.Component {
-
-  componentDidMount(): void {
+const Demo = () => {
+  const [index, setIndex] = useState(-1) as any;
+  useEffect(() => {
     Platform.OS === 'android' && SplashScreen.hide();
-  }
+  });
 
-  render() {
-    return <FlutterAnimation/>
+  function renderListView() {
+    if (!DemoRouters.length) {
+      console.error('No Demo Component List Data!!')
+    }
+    return (
+      <ScrollView>
+        <MapView onItemPress={index => setIndex(index)} />
+      </ScrollView>
+    )
   }
-}
+  function renderContent() {
+    if (index === -1) {
+      return renderListView();
+    } else {
+      return <NavigationView initialRouteName={DemoRouters[index].displayName} />;
+    }
+  }
+  function renderHeader() {
+    const cmp = DemoRouters[index];
+    const params = {
+      title: cmp ? cmp.displayName : 'Demo',
+      leftButton: cmp ? undefined : null,
+      onBackPress: () => setIndex(-1),
+    };
+    return <NavigationBar {...params} />;
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      {renderHeader()}
+      {renderContent()}
+    </View>
+  )
+};
+
 
 export default Demo
---
+++ demo/DemoRouters.ts
@@ -0,0 +1,62 @@
+// @ts-ignore
+import TabViewExampleList from './react-native-tab-view/TabViewExampleList';
+import SortableListExample from './sort-and-swipe/react-native-sortable-list/example/SortableListExample';
+import SortableListViewExample from './sort-and-swipe/react-native-sortable-list/example/SortableListViewExample';
+import DragableListExample from './sort-and-swipe/react-native-sortable-list/example/DragableListExample';
+import SwipeableExample from './sort-and-swipe/react-native-swipeable/Example'
+import CustomSortAndSwipeList from './sort-and-swipe/react-native-sort-and-swipe-list/src/CustomSortAndSwipeList'
+import HumanShowcaseScreen from './react-native-typography/HumanShowcaseScreen'
+import MaterialShowcaseScreen from './react-native-typography/MaterialShowcaseScreen'
+// @ts-ignore
+import TypographyExample from './react-native-typography/TypographyExample'
+import Header from './Header/'
+import KeyboardDemoScreen from './keyboard-input/demoScreen';
+import RecycleTestComponent from './recyclerlistview/RecycleTestComponent'
+import StickySample from './recyclerlistview/StickySample'
+import NativeMaterialDialog from './material-dialog/NativeMaterialDialog'
+// @ts-ignore
+import SyanImagePicker from './image-picker/SyanImagePicker'
+import OfflineDemo from './offline/OfflineDemo'
+import RNShareDemo from './Share/RNShareDemo'
+import RNCShareDemo from './Share/RNCShareDemo'
+import Count1 from './hooks/Count1'
+import Count2 from './hooks/Count2'
+import Count3 from './hooks/Count3'
+import LightboxDemo from './light-demo/LightboxDemo'
+import ReanimatedCollapsibleNavbar from './collapsible/reanimated-collapsible-navbar/ReanimatedCollapsibleNavbar';
+import ReactNativeScrollableHeader from './collapsible/react-native-scrollable-header/ReactNativeScrollableHeader';
+// import ExpoExample from './expo/ExpoExample'
+// import RNScreensExample from './rn-screens/App';
+import SpecificPlatformExample from './specific-platform';
+// import SpecificPlatformTSExample from './specific-platform-ts';
+import FlutterAnimation from './fluttter-animations/'
+import MerryPhotoViewer from './@merryjs/photo-viewer/MerryPhotoViewer';
+
+export default [
+  // TabViewExampleList,
+  SortableListExample,
+  SortableListViewExample,
+  DragableListExample,
+  SwipeableExample,
+  CustomSortAndSwipeList,
+  HumanShowcaseScreen,
+  MaterialShowcaseScreen,
+  // TypographyExample,
+  Header,
+  KeyboardDemoScreen,
+  RecycleTestComponent,
+  StickySample,
+  NativeMaterialDialog,
+  OfflineDemo,
+  RNShareDemo,
+  RNCShareDemo,
+  Count1,
+  Count2,
+  Count3,
+  LightboxDemo,
+  ReanimatedCollapsibleNavbar,
+  ReactNativeScrollableHeader,
+  SpecificPlatformExample,
+  MerryPhotoViewer,
+  FlutterAnimation,
+];
